5ab55c13a2983d1fb6e3f1f0259de020dfc3d60e
iviczian <viczian.istvan@gmail.com>
2021-05-13 13:40:43 +0200

    Mockito unit testing

--- employees-app/src/main/java/employees/TransferCommand.java
+++ employees-app/src/main/java/employees/TransferCommand.java
@@ -1,8 +1,12 @@
 package employees;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TransferCommand {
 
     private long sourceId;
---
+++ employees-app/src/test/java/employees/TransferServiceTest.java
@@ -0,0 +1,29 @@
+package employees;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.argThat;
+
+@ExtendWith(MockitoExtension.class)
+public class TransferServiceTest {
+
+    @Mock
+    EmployeesService employeesService;
+
+    @InjectMocks
+    TransferService transferService;
+
+    @Test
+    void testTransfer() {
+        transferService.transfer(new TransferCommand(1, 2, 500));
+
+        Mockito.verify(employeesService).credit(argThat(c -> c.getEmployeeId() == 1 && c.getDiff() == -500));
+        Mockito.verify(employeesService).credit(argThat(c -> c.getEmployeeId() == 2 && c.getDiff() == 500));
+
+    }
+}
